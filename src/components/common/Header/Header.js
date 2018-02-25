13538e3590cdb94c69ae1db93d09fe5192c0fb25
YongPilMoon <myp7@urbanbase.com>
2018-02-25 21:01:33 +0900

    Header component 추가

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import styles from './Header.scss';
-import classNames from 'classNames/bind';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-const Header = () => {
-  return (
-    <div className={cx('header')}>
-      Header
+const Header = () => (
+  <header className={cx('header')}>
+    <div className={cx('header-content')}>
+      <div className={cx('brand')}>
+        <Link to="/">reactblog</Link>
+      </div>
+      <div className={cx('right')}>
+        {/* 조건에 따라 버튼 랜더링 */}
+        오른쪽
+      </div>
     </div>
-  );
-};
+  </header>
+);
 
 export default Header;
