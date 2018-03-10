bd275d154f76bc4dfa96ab60af6eca6d42713439
joy.hj <joy.hj@kakaocorp.com>
2018-03-10 18:38:28 +0900

    마크다운 에디터 구현 완료(7장)

--- src/components/editor/EditorPane/EditorPane.js
+++ src/components/editor/EditorPane/EditorPane.js
@@ -72,12 +72,12 @@ class EditorPane extends Component {
             <input className={cx('title')} placeholder="제목을 입력하세요" name="title" value={title}
                    onChange={handleChange}
             />
-            <div className={cx('code-editor')} ref={ref=>this.editor=ref}></div>
+            <div className={cx('code-editor')} ref={ref=>this.editor=ref}> </div>
             <div className={cx('tags')}>
             <div className={cx('description')}>태그</div>
                 <input name="tags" placeholder="태그를 입력하세요 (쉼표로 구분)" value={tags} onChange={handleChange}/>
             </div>
-        </div>
+</div>
         );
     }
 }
