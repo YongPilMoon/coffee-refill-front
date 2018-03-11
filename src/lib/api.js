0982f7a8135e81e7a7bb68138c8f5c2c0ead03a5
YongPilMoon <myp7@urbanbase.com>
2018-03-11 15:48:00 +0900

    7-2: post 모듈 작성하기

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,3 +4,4 @@ const API_SERVER = '127.0.0.1';
 const API_SERVER_PORT = '7070';
 
 export const writePost = ({title, body, tags}) => axios.post(`${API_SERVER}:${API_SERVER_PORT}/write`, { title, body, tags });
+export const getPost = (id) => axios.get(`/api/posts/${id}`);
